e1b13ce28bb26af7282b1e349da907feb82b76c1
fap233 <fellipe306@gmail.com>
2025-11-06 16:53:46 -0300

    feat(backend): Implementado POST /tasks com validação CORS

--- backend/handlers.go
+++ backend/handlers.go
@@ -22,16 +22,45 @@ func getNextTaskID() string {
 	return strconv.Itoa(taskIDCounter)
 }
 
+// CRUD - CREATE
+
+func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
+		log.Printf("Error decoding JSON: %v", err)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	if newTask.Title == "" {
+		http.Error(w, "Title is required", http.StatusBadRequest)
+		return
+	}
+	newTask.ID = getNextTaskID()
+	newTask.Status = ToDoStatus
+
+	mu.Lock()
+	store[newTask.ID] = newTask
+	mu.Unlock()
+
+	log.Printf("Task successfully created: ID %s, Title %s", newTask.ID, newTask.Title)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
 func tasksHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		getTasksHandler(w, r)
-
+	case http.MethodPost:
+		createTaskHandler(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
+// CRUD - GET
 func getTasksHandler(w http.ResponseWriter, _ *http.Request) {
 	mu.RLock()
 	defer mu.RUnlock()
--- backend/main.go
+++ backend/main.go
@@ -6,17 +6,37 @@ import (
 	"net/http"
 )
 
-func main() {
-	// hello world rote
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "KanbanApi already working!")
-	})
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-	// chore rote
-	http.HandleFunc("/tasks", tasksHandler)
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "KanbanApi already working!")
+}
+
+func main() {
+	// multiplexer
+	mux := http.NewServeMux()
+
+	// mux handler register
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/tasks", tasksHandler)
+
+	// middleware
+	corsHandler := enableCORS(mux)
 
 	// server initialization
 	log.Println("Starting server on :8080")
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
