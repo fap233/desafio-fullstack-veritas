3c7eb4c69061d23792ce92f18d8ea74f95d0357a
fap233 <fellipe306@gmail.com>
2025-11-06 13:47:47 -0300

    feat(backend): Modelo de dados de tarefas e de status definidos

---
+++ backend/models.go
@@ -0,0 +1,16 @@
+package main
+
+type Status string
+
+const (
+	ToDoStatus       Status = "A Fazer"
+	InProgressStatus Status = "Em Progresso"
+	DoneStatus       Status = "Concluídas"
+)
+
+type Task struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description,omitempty"`
+	Status      Status `json:"status"`
+}
