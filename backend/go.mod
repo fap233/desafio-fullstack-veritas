24f2d2c1fbe10b3a92cd23e81754489d7f5c5bb0
fap233 <fellipe306@gmail.com>
2025-11-06 13:39:08 -0300

    feat(backend): Módulo Go e HTTP server básico inicializados

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/fap233/desafio-fullstack-veritas/backend
+
+go 1.25.3
---
+++ backend/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "KanbanApi already working!")
+	})
+
+	log.Println("Starting server on :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
