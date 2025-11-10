0a6a47a424ccacd023d9b4128cf365744dc3fe9d
fap233 <fellipe306@gmail.com>
2025-11-10 00:41:03 -0300

    fix(ui/ux):adicionada responsividade nas colunas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import KanbanBoard from "./components/KanbanBoard";
 
 function App() {
 	return (
-		<main className="flex min-h-screen w-full overflow-x-auto overflow-y-hidden bg-gray-900 p-4">
+		<main className="flex min-h-screen w-full overflow-y-auto md:overflow-x-auto md:overflow-y-hidden bg-gray-900 p-4">
 			<KanbanBoard />
 		</main>
 	);
